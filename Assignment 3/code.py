46d1d26d5781bd9eca2d9412bfe5e42c53a0c3e7
SindriTJ18 <sindritj18@ru.is>
2021-11-25 17:43:34 +0000

    Assignment3

---
+++ Assignment 3/code.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+
+classes = []
+
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+print(classes)
+
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow('Image', img)
+#     cv2.waitKey(1)
